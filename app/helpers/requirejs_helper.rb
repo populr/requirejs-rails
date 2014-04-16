b3b9aabdfe2465618101829787e4b384c7f6ac88
daniel-nelson <daniel.wolcott.nelson@gmail.com>
2014-04-16 13:04:19 -0500

    It appears requirejs_rails is now using config.action_controller.asset_host instead of the documented config.requirejs.run_config['baseUrl']
    https://github.com/jwhitley/requirejs-rails/issues/105

--- app/helpers/requirejs_helper.rb
+++ app/helpers/requirejs_helper.rb
@@ -97,6 +97,7 @@ module RequirejsHelper
 
   def baseUrl(js_asset)
     js_asset_path = javascript_path(js_asset)
+    js_asset_path = "https://#{js_asset_path}" unless js_asset_path =~ /^https?:\/\//
     uri = URI.parse(js_asset_path)
     asset_host = uri.host && js_asset_path.sub(uri.request_uri, '')
     [asset_host, Rails.application.config.assets.prefix].join
